e2ad0ac37962b1f6c11870673397ed590e209e44
Umgoel <76695806+Umgoel@users.noreply.github.com>
2021-05-10 10:15:01 +0530

    Create largerof2.c

---
+++ lab1/largerof2.c
@@ -0,0 +1,16 @@
+//find larger of 2 nos
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a : ");
+	scanf("%d",&a);
+	printf("enter b : ");
+	scanf("%d",&b);
+	if(a>b)
+	printf("a is greater\n");
+	else if(b>a)
+	printf("b is greater\n");
+	else 
+	printf("equal\n");
+}
