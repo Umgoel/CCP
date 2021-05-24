b1769ba6917b5c71e1f94245903038e7020ce7ab
Umgoel <76695806+Umgoel@users.noreply.github.com>
2021-05-24 09:55:52 +0530

    Add files via upload

---
+++ lab2/lab2_eval/dist.c
@@ -0,0 +1,17 @@
+//2. dist between two points
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+	float x1, y1, x2, y2, f;
+	printf("Enter x1 : ");
+	scanf("%f", &x1);
+	printf("Enter y1 : ");
+	scanf("%f", &y1);
+    printf("Enter x2 : ");
+	scanf("%f", &x2);
+	printf("Enter y2 : ");
+	scanf("%f", &y2);
+	f = pow((x2-x1),2)+pow((y2-y1),2);
+	printf("Distance : %.2f \n", sqrt(f));
+}
